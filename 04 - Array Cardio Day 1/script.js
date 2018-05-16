ccedc12dd5a76f042bd876c70cc11b1502627320
Isak Mladenoff <imladenoff@gmail.com>
2018-05-15 18:21:37 -0700

    Complete Day 4

---
+++ 04 - Array Cardio Day 1/script.js
@@ -0,0 +1,113 @@
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
+
+// Some data we can work with
+
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+const boulevardArray = ['Boulevards of Paris', 'City walls of Paris', 'Thiers wall', 'Wall of Charles V', 'Wall of Philip II Augustus', 'City gates of Paris', "Haussmann's renovation of Paris", 'Boulevards of the Marshals', 'Boulevard Auguste-Blanqui', 'Boulevard Barbès', 'Boulevard Beaumarchais', "Boulevard de l'Amiral-Bruix", 'Boulevard des Capucines', 'Boulevard de la Chapelle', 'Boulevard de Clichy', 'Boulevard du Crime', 'Boulevard Haussmann', "Boulevard de l'Hôpital", 'Boulevard des Italiens', 'Boulevard de la Madeleine', 'Boulevard de Magenta', 'Boulevard Montmartre', 'Boulevard du Montparnasse', 'Boulevard Raspail', 'Boulevard Richard-Lenoir', 'Boulevard de Rochechouart', 'Boulevard Saint-Germain', 'Boulevard Saint-Michel', 'Boulevard de Sébastopol', 'Boulevard de Strasbourg', 'Boulevard du Temple', 'Boulevard Voltaire', 'Boulevard de la Zone'];
+
+const inventorLifespan = inventor => inventor.passed - inventor.year;
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+
+const sixteenthCenturyInventors = inventors.filter(inventor =>
+  inventor.year > 1499 && inventor.year < 1600,
+);
+
+console.table(sixteenthCenturyInventors);
+
+// Array.prototype.map()
+// 2. Give us an array of the inventors' first and last names
+
+const inventorNames = inventors.map(inventor =>
+  `${inventor.first} ${inventor.last}`,
+);
+
+console.table(inventorNames);
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+
+const inventorsByYear = inventors.slice(0).sort((a, b) =>
+  a.year - b.year,
+);
+
+console.table(inventorsByYear);
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+
+const totalInventorLifespans = inventors.reduce(
+  (accum, inventor) => accum + inventorLifespan(inventor),
+  0,
+);
+
+console.log(totalInventorLifespans);
+
+// 5. Sort the inventors by years lived
+
+const inventorsByLifespan = inventors.slice(0).sort((a, b) =>
+  inventorLifespan(a) - inventorLifespan(b),
+);
+
+console.table(inventorsByLifespan);
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+const isSubString = function isSubString(string, subString) {
+  for (let idx = 0; idx < string.length - subString.length; idx += 1) {
+    if (string.slice(idx, idx + subString.length) === subString) { return true; }
+  }
+  return false;
+};
+
+const boulevardsDeDe = boulevardArray.filter(street => isSubString(street, 'de'));
+
+console.table(boulevardsDeDe);
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+const getFirstName = name => name.split(', ')[1];
+
+// IM: I have sorted by first name because, unless I am missing something,
+// sorting by last name here is trivial (it is just sorting the strings)
+
+const peopleByFirstName = people.slice(0).sort((a, b) =>
+  (getFirstName(a) < getFirstName(b) ? -1 : 1),
+);
+
+// const peopleByLastname = people.slice(0).sort();
+
+console.table(peopleByFirstName);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+
+const vehicleCounts = data.reduce((accum, vehicle) => {
+  if (accum[vehicle] === undefined) {
+    accum[vehicle] = 0;
+  }
+  accum[vehicle] += 1;
+  return accum;
+}, {});
+
+console.log(vehicleCounts);
